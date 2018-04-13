f16ceaeec5abae9bbe07bf0376f9f5b3626cbcea
Neven Miculinic <neven.miculinic@gmail.com>
2018-04-13 16:35:10 +0200

    Added stuff

--- 02-first_steps/README.md
+++ 02-first_steps/README.md
@@ -1,3 +1,4 @@
 Tasks:
     * Deploy the pod onto the minikube
     * Find pod's IP address, hosting node, containers, labels, resource usage.
+    * Open dashboard `minikube dashboard`
--- 02-first_steps/main.go
+++ 02-first_steps/main.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Panicln(err)
+	}
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		log.Printf("Got request from %v for %v", request.RemoteAddr, request.RequestURI)
-		fmt.Fprintln(writer, "hello world")
+		fmt.Fprintln(writer, "hello world from", hostname)
 	})
 	log.Println("Listening to :8080")
 	http.ListenAndServe(":8080", nil)
